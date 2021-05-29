40c105e81d589ce0c13a7817c3f5465cc44a5516
Satyam <satyamshashankkaran.ssk@gmail.com>
2021-05-29 10:38:08 +0530

    first commit

---
+++ HandTrackingModule.py
@@ -0,0 +1,19 @@
+import cv2
+import time
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    print(results.multi_hand_landmarks)
+
+    
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
+

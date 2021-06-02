7f8abcf46bc8b33d288fa11fffc10dadee63c8fc
Satyam <satyamshashankkaran.ssk@gmail.com>
2021-06-02 11:01:11 +0530

    Min code required

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -18,6 +18,15 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+
+            for id, lm in enumerate(handLms.landmark):
+                h,w,c = img.shape
+                cx, cy  = int(lm.x*w), int(lm.y*h)
+                print(id,cx,cy)
+
+                if id == 4:
+                    cv2.circle(img,(cx,cy),20,(255,0,255),cv2.FILLED)
+
             mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
 
 
